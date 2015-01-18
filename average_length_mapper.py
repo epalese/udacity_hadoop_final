381de0cd2328d9a7e15733acbc9fff95849bdd73
epalese <leleplx@gmail.com>
2015-01-18 07:51:22 -0500

    avg length

---
+++ average_length_mapper.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+# Format of each line is:
+# id title tagnames author_id body node_type parent_id abs_parent_id added_at score state_string last_edited_id last_activity_by_id last_activity_at active_revision_id extra extra_ref_id extra_count marked
+# fields are \t separated
+
+import sys, csv
+from datetime import datetime
+
+reader = csv.reader(sys.stdin, delimiter='\t')
+# writer = csv.writer(sys.stdout, delimiter='\t', quotechar='"', quoting=csv.QUOTE_ALL)
+next(reader, None)	# skip header
+
+for line in reader:
+	if (line != None and len(line) == 19):
+		node_type = line[5]
+		if line[5] == 'question':
+			print "{0}\t{1}\t{2}".format(line[0], line[5], len(line[4].replace('\N', '')))
+		if line[5] == 'answer':
+			print "{0}\t{1}\t{2}".format(line[6], line[5], len(line[4].replace('\N', '')))
---
+++ average_length_reducer.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+
+import sys
+
+currentNodeId = None
+questionLength = 0
+countAnswerLength = 0
+countAnswers = 0
+
+# The mapper output will be a tab separated list: [authorid]\t[hour]\t[count]
+# The count field will contain always a value of 1 unless a combiner is used that will group results by authorid and hour   
+
+for line in sys.stdin:
+	data = line.strip().split("\t")
+    	if len(data) != 3:
+		# Something has gone wrong. Skip this line.
+		continue
+
+	nodeId, type, length = data
+	if (not nodeId.isdigit()) or (not length.isdigit()):
+		# Something has gone wrong. Skip this line.
+		continue 
+		
+	if currentNodeId and currentNodeId != nodeId:
+		if countAnswers > 0:
+			avgAnswerLength	= countAnswerLength / countAnswers
+		else:
+			avgAnswerLength = 0
+		print currentNodeId, "\t", questionLength, "\t", avgAnswerLength
+		questionLength = 0
+		countAnswers = 0
+		countAnswerLength = 0
+	
+	currentNodeId = nodeId
+	if type == 'question':
+		questionLength = length
+	if type == 'answer':
+		countAnswers += 1
+		countAnswerLength += float(length)
+
+if currentNodeId != None:
+	if type	== 'question':
+                questionLength = length
+        if type	== 'answer':
+		countAnswers += 1
+		countAnswerLength += float(length)
+	
+	if countAnswers	> 0:
+		avgAnswerLength = countAnswerLength / countAnswers
+	else:
+		avgAnswerLength = 0	
+	print currentNodeId, "\t", questionLength, "\t", avgAnswerLength
