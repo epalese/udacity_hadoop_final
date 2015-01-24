e7e3cc4be412eaa90da8d8a2417f75ef059b6f8e
epalese <epalese@users.noreply.github.com>
2015-01-24 22:34:23 +0000

    Update average_length_mapper.py

--- average_length_mapper.py
+++ average_length_mapper.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python
 
-# Format of each line is:
-# id title tagnames author_id body node_type parent_id abs_parent_id added_at score state_string last_edited_id last_activity_by_id last_activity_at active_revision_id extra extra_ref_id extra_count marked
-# fields are \t separated
+# input format:
+# "id"\t"title"\t"tagnames"\t"author_id"\t"body"\t"node_type"\t"parent_id"\t"abs_parent_id"\t"added_at"\t"score"\t"state_string"\t"last_edited_id"\t"last_activity_by_id"\t"last_activity_at"\t"active_revision_id"\t"extra"\t"extra_ref_id"\t"extra_count"marked"
 
 import sys, csv
 from datetime import datetime
