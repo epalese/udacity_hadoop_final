eee7a08f98cb419b13c8debcf87642d689617aa8
epalese <epalese@users.noreply.github.com>
2015-01-24 22:42:04 +0000

    Update popular_tags_mapper.py

--- popular_tags_mapper.py
+++ popular_tags_mapper.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python
 
-# Format of each line is:
-# id title tagnames author_id body node_type parent_id abs_parent_id added_at score state_string last_edited_id last_activity_by_id last_activity_at active_revision_id extra extra_ref_id extra_count marked
-# fields are \t separated
+# input format:
+# "id"\t"title"\t"tagnames"\t"author_id"\t"body"\t"node_type"\t"parent_id"\t"abs_parent_id"\t"added_at"\t"score"\t"state_string"\t"last_edited_id"\t"last_activity_by_id"\t"last_activity_at"\t"active_revision_id"\t"extra"\t"extra_ref_id"\t"extra_count"marked"
 
 import sys, csv
 from datetime import datetime
 
 reader = csv.reader(sys.stdin, delimiter='\t')
-# writer = csv.writer(sys.stdout, delimiter='\t', quotechar='"', quoting=csv.QUOTE_ALL)
 next(reader, None)	# skip header
 
 for line in reader:
 	if (line != None and len(line) == 19):
 		node_type = line[5]
+		# Consider tags only from question nodes
 		if line[5] == 'question':
 			tags = line[2].split(' ')
 			for tag in tags:
